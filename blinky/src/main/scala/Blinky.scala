a6df20bea0751e3ac1031780557340f7ed09754b
Carlos de Paula <me@carlosedp.com>
2020-11-26 20:04:05 -0300

    Merge Blinky and toplayer into same file

--- blinky/src/main/scala/Blinky.scala
+++ blinky/src/main/scala/Blinky.scala
@@ -22,6 +22,19 @@ class Blinky(freq: Int) extends Module {
   io.led1 := ~blkReg
 }
 
+// Blinking LED top layer
+class BlinkyTop extends Module {
+  val io = IO(new Bundle {
+    val led0 = Output(UInt(1.W))
+    val led1 = Output(UInt(1.W))
+  })
+
+  val b = Module(new Blinky(25000000))
+
+  b.io <> io
+  b.reset <> ~reset.asBool()
+}
+
 /**
   * An object extending App to generate the Verilog code.
   */
