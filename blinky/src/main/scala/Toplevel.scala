d7886f0f3ac6075659c13ac33feaf0c77ca807e1
Carlos de Paula <me@carlosedp.com>
2021-04-13 20:44:34 -0300

    Add support for Digilent ArtyA7-35T board
    Signed-off-by: Chandler Jearls <cjearls@vt.edu>

--- blinky/src/main/scala/Toplevel.scala
+++ blinky/src/main/scala/Toplevel.scala
@@ -2,7 +2,7 @@ import chisel3._
 import scalautils.ParseArguments
 
 // Blinking LED top layer
-class Toplevel(board: String) extends Module {
+class Toplevel(board: String, invReset: Boolean = true) extends Module {
   val io = IO(new Bundle {
     val led0 = Output(Bool())
     val led1 = Output(Bool())
@@ -21,7 +21,12 @@ class Toplevel(board: String) extends Module {
   // Connect Clock and Reset(inverted)
   pll.io.clki := clock
   bl.clock := pll.io.clko
-  bl.reset := ~reset.asBool()
+  // Define if reset should be inverted based on board switch
+  if (invReset) {
+    bl.reset := ~reset.asBool()
+  } else {
+    bl.reset := reset
+  }
 }
 
 // The Main object extending App to generate the Verilog code.
@@ -30,13 +35,15 @@ object Toplevel extends App {
   // Parse command line arguments and extract required parameters
   // pass the input arguments and a list of parameters to be extracted
   // The funcion will return the parameters map and the remaining non-extracted args
-  val (params, chiselargs) = ParseArguments(args, List("board"))
+  val (params, chiselargs) = ParseArguments(args, List("board", "invreset"))
   val board: String =
     params.getOrElse("board", throw new IllegalArgumentException("The '-board' argument should be informed."))
+  val invReset: Boolean =
+    params.getOrElse("invreset", "true").toBoolean
 
   // Generate Verilog
   (new chisel3.stage.ChiselStage).emitVerilog(
-    new Toplevel(board),
+    new Toplevel(board, invReset),
     chiselargs
   )
 }
