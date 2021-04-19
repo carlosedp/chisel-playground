2403b062695d0cf7060a7e49433f6626ed546c1f
Carlos de Paula <me@carlosedp.com>
2021-04-19 12:35:41 -0300

    Improve proginfo

--- blinky/scripts/proginfo.py
+++ blinky/scripts/proginfo.py
@@ -6,21 +6,22 @@ system_name = "carlosedp_demo_chiselblinky_0"
 print("")
 print("Build was completed")
 print("To program the board run:")
-print("")
 if sys.argv is None:
     exit
 if sys.argv[1] == 'prog-artya7-35t':
     interfaceConfig = os.path.join(os.getcwd(), "../src/"+system_name, "openocd/digilent-hs1.cfg")
     boardConfig = os.path.join(os.getcwd(), "../src/"+system_name, "openocd/xilinx-xc7.cfg")
     bitFile = os.path.join(os.getcwd(), "Toplevel.bit")
-    print("openocd -f {} -f {} -c \"transport select jtag;init; pld load 0 {}; exit\"".format(interfaceConfig, boardConfig, bitFile))
+    print("> openocd -f {} -f {} -c \"transport select jtag;init; pld load 0 {}; exit\"".format(interfaceConfig, boardConfig, bitFile))
 elif sys.argv[1] == 'prog-ulx3s':
-    print("ujprog {}".format(os.path.join(os.getcwd(), system_name+".bit")))
+    print("> ujprog {}".format(os.path.join(os.getcwd(), system_name+".bit")))
+    print("or using openocd with:")
+    interfaceConfig = os.path.join(os.getcwd(), "../src/"+system_name, "openocd/ft231x.cfg")
+    boardConfig = os.path.join(os.getcwd(), "../src/"+system_name, "openocd/LFE5U-85F.cfg")
+    bitFile = os.path.join(os.getcwd(), system_name+".svf")
+    print("> openocd -f {} -f {} -c \"transport select jtag; init; scan_chain; svf -quiet -progress {}; exit\"".format(interfaceConfig, boardConfig, bitFile))
     print("Other programming options are listed here https://github.com/emard/ulx3s/blob/master/doc/MANUAL.md#programming-options")
-elif sys.argv[1] == 'dfu-util-fomu':
-    print("Download and install dfu-util from http://dfu-util.sourceforge.net/")
-    print("dfu-util -e -d 1209:5bf0 -D {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 elif sys.argv[1] == 'dfu-util':
     print("Download and install dfu-util from http://dfu-util.sourceforge.net/")
-    print("dfu-util -e -d 1209:5bf0 -D {}".format(os.path.join(os.getcwd(), system_name+".bit")))
+    print("> dfu-util -e -d 1209:5bf0 -D {}".format(os.path.join(os.getcwd(), system_name+".bin")))
 print("")
