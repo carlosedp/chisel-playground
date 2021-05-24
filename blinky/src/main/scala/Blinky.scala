2470293a81b05609ba864b2536452e27b9c71a2d
Carlos de Paula <me@carlosedp.com>
2021-05-24 18:34:51 -0300

    Add PWM and breathe functions. Use it on one LED

--- blinky/src/main/scala/Blinky.scala
+++ blinky/src/main/scala/Blinky.scala
@@ -11,13 +11,59 @@ class Blinky(freq: Int) extends Module {
     val led2 = Output(Bool())
   })
 
-  val led                         = RegInit(0.U(1.W))
-  val (counterValue, counterWrap) = Counter(true.B, freq / 2)
+  // Alternate blink leds every second
+  val led              = RegInit(0.U(1.W))
+  val (_, counterWrap) = Counter(true.B, freq / 2)
   when(counterWrap) {
     led := ~led
   }
 
   io.led0 := led
   io.led1 := ~led
-  io.led2 := led
+
+  // 1 beathe cycle every second
+  io.led2 := breathe(freq, 1000)
+
+  //-------------- Utility Functions --------------
+  /**
+   * pwmAbs creates a PWM output from a 0 to 100 Duty Cycle
+   *
+   * @param dutyCycle UInt from 0 to 100 (as in percent)
+   * @return Bool output
+   */
+  def pwmAbs(dutyCycle: UInt) = {
+    // Divide input frequency by 1024 to avoid switchind the LEDs at high freq
+    val clockDiv = RegInit(0.U(log2Ceil(1024).W))
+    clockDiv := clockDiv + 1.U
+    val cnt = RegInit(0.U(7.W))
+    val out = RegInit(false.B)
+    when(clockDiv === 0.U) {
+      cnt := Mux(cnt === 100.U, 0.U, cnt + 1.U)
+      out := dutyCycle > cnt
+    }
+    out
+  }
+  // To set a specific value (10% duty cycle)
+  // io.led2 := pwmAbs(10.U)
+
+  /**
+   * breathe creates a gradual off-on-off cycle with pwmAbs function
+   *
+   * @param freq is the module clock frequency
+   * @param speed in miliseconts for a full off-on-off cycle
+   * @return Bool output
+   */
+  def breathe(freq: Int, speed: Int) = {
+    val pwmRange = 100
+    val pwm      = RegInit(0.U(log2Ceil(pwmRange).W))
+    val upReg    = RegInit(true.B)
+    val (_, cw)  = Counter(true.B, (freq * (speed.toFloat / 1000) / 2 / pwmRange).toInt)
+    when(cw) {
+      when(pwm < pwmRange.U && upReg)(pwm := pwm + 1.U)
+        .elsewhen(pwm === pwmRange.U && upReg)(upReg := false.B)
+        .elsewhen(pwm > 0.U && !upReg)(pwm := pwm - 1.U)
+        .otherwise(upReg := true.B)
+    }
+    pwmAbs(pwm)
+  }
 }
