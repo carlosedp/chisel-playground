6fa0232b9bd5b65c8760918a08e2194a5ca962e6
Carlos de Paula <me@carlosedp.com>
2021-09-21 20:23:14 -0300

    Update Qomu clock source

--- blinky/src/main/resources/pll_qomu.v
+++ blinky/src/main/resources/pll_qomu.v
@@ -2,18 +2,18 @@
 module PLL0
 (
     input clki, // No input since the MCU generates the clock
-    output clko, // 25 MHz output from the MCU clock generator
+    output clko, // 24 MHz output from the MCU clock generator (bootloader has 24Mhz and 48Mhz clocks)
     output lock
 );
 
 /* CPU <-> FPGA interface
     * here only one of the two clock sources
     * and corresponding reset signal are used
-    * Sys_Clk0/Sys_Clk0_Rst: C16 clock domain
-    * Sys_Clk1/Sys_Clk1_Rst: C21 clock domain
+    * Sys_Clk0/Sys_Clk0_Rst: C16 clock domain (default as 24Mhz from bootloader)
+    * Sys_Clk1/Sys_Clk1_Rst: C21 clock domain (default as 48Mhz from bootloader)
     */
 qlal4s3b_cell_macro u_qlal4s3b_cell_macro (
-    .Sys_Clk0 (clko),
+    .Sys_Clk0 (clko), // we expect 25Mhz but the default 24Mhz from MCU is close :)
     .Sys_Clk0_Rst (),
     .Sys_Clk1     (),
     .Sys_Clk1_Rst ()
